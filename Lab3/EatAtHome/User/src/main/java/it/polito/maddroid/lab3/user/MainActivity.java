27a33709d1ca4f57a0087c4837fad59da35f1ff9
Francesco Cappio Borlino <francesco.cappioborlino@tuta.io>
2019-04-16 12:30:08 +0200

    Modified also User's and rider's mainActivities to start diretly the splash screen activity

--- Lab3/EatAtHome/User/src/main/java/it/polito/maddroid/lab3/user/MainActivity.java
+++ Lab3/EatAtHome/User/src/main/java/it/polito/maddroid/lab3/user/MainActivity.java
@@ -1,8 +1,11 @@
 package it.polito.maddroid.lab3.user;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import it.polito.maddroid.lab3.common.SplashScreenActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Intent i = new Intent(getApplicationContext(), SplashScreenActivity.class);
+        startActivity(i);
     }
 }
