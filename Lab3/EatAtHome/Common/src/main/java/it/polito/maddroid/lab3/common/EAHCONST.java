7316d94b315041aac44c9f5176f144233c415e65
Francesco Cappio Borlino <francesco.cappioborlino@tuta.io>
2019-04-18 11:22:45 +0200

    Added some const

--- Lab3/EatAtHome/Common/src/main/java/it/polito/maddroid/lab3/common/EAHCONST.java
+++ Lab3/EatAtHome/Common/src/main/java/it/polito/maddroid/lab3/common/EAHCONST.java
@@ -8,4 +8,31 @@ public class EAHCONST {
     public static String LAUNCH_APP_RESTAURATEUR = "LAUNCH_APP_RESTAURATEUR";
     public static String LAUNCH_APP_USER = "LAUNCH_APP_USER";
     public static String LAUNCH_APP_RIDER = "LAUNCH_APP_RIDER";
+    
+    // main subtrees
+    final static String USERS_SUB_TREE = "users";
+    final static String RESTAURANTS_SUB_TREE = "restaurants";
+    
+    // users subtree nodes fields
+    final static String USERS_NAME = "username";
+    final static String USERS_UUID = "uuid";
+    final static String USERS_TYPE = "usertype";
+    
+    // restaurants subtree nodes fields
+    final static String RESTAURANT_NAME = "name";
+    final static String RESTAURANT_ADDRESS = "address";
+    final static String RESTAURANT_DESCRIPTION = "decription";
+    
+    
+    public static String generatePath(String... args) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < args.length; ++i) {
+            sb.append(args[i]);
+            if (i != args.length -1)
+                sb.append("/");
+        }
+        
+        return sb.toString();
+    }
 }
