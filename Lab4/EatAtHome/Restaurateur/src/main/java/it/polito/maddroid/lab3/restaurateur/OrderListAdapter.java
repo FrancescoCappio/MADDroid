1cebd5b1424170aa76020dd853dd55cfa9e63e7a
Francesco Cappio Borlino <francesco.cappioborlino@tuta.io>
2019-05-17 11:49:25 +0200

    Moved choose rider activity to Common so that we can call it also from rider

--- Lab4/EatAtHome/Restaurateur/src/main/java/it/polito/maddroid/lab3/restaurateur/OrderListAdapter.java
+++ Lab4/EatAtHome/Restaurateur/src/main/java/it/polito/maddroid/lab3/restaurateur/OrderListAdapter.java
@@ -98,7 +98,7 @@ public class OrderListAdapter extends ListAdapter<Order, OrderListAdapter.MyView
                 case CONFIRMED:
                     cardView.setCardBackgroundColor(ContextCompat.getColor(itemView.getContext(),R.color.eah_green_alert));
                     break;
-                case COMPLETED:
+                default:
                     cardView.setCardBackgroundColor(ContextCompat.getColor(itemView.getContext(),R.color.eah_white));
                     break;
             }
